cfba59abb17e505027b0a02360a109bff9ac3a87
pkpalakonda <37602412+pkpalakonda@users.noreply.github.com>
2019-11-20 15:31:51 -0800

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -92,7 +92,12 @@ namespace ClaimsApi
             // Enable middleware to serve Swagger-ui, specifying Swagger JSON end-point.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("../swagger/v1/swagger.json", "Claims API");
+		    string swaggerJsonBasePath = string.IsNullOrWhiteSpace(options.RoutePrefix) ? "." : "..";
+		    foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/{description.GroupName}/swagger.json", description.GroupName.ToUpperInvariant());
+                }
+          //      c.SwaggerEndpoint("../swagger/v1/swagger.json", "Claims API");
 		   
             });
 		
